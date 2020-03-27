ddd92bb8821f7839df86fc35181e6fc830949a61
39340 <39340@PC-MARIUSZ>
2020-03-27 17:12:23 +0100

    Aggiungere i file di progetto.

---
+++ Report Creator/Logic/CSVReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Report_Creator.Logic
+{
+
+    public abstract class CSVReader<T>
+    {
+
+        public TextFieldParser TxtFieldParser { get; private set; }
+
+        public CSVReader(string path, string [] delimiters)
+        {
+            TxtFieldParser = new TextFieldParser(path);
+            TxtFieldParser.Delimiters = delimiters;
+            TxtFieldParser.TrimWhiteSpace = true;
+            TxtFieldParser.HasFieldsEnclosedInQuotes = true;
+        }
+
+        public abstract List<T> Items();
+    }
+}
---
+++ Report Creator/Logic/Practice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report_Creator.Logic
+{
+    class Practice
+    {
+
+        #region Header variables
+        // ID of the DDT in grid as practice
+        public int DDTID { get; set; }
+
+        // Client name
+        public string Client { get; set; }
+
+        // Delivery note reference - number
+        public string DDTReference { get; set; }
+
+        // DDT date creation
+        public DateTime DDTDate { get; set; }
+
+        // A list of RD present
+        public List<string> RDs { get; set; }
+        #endregion
+
+        #region Default constructor
+        public Practice(string idDDT, string clientName, string documentReference, string documentDate)
+        {
+            DDTID = Int32.Parse(idDDT);
+            Client = clientName;
+            DDTReference = documentReference;
+            DDTDate = DateTime.Parse(documentDate);
+        }
+        #endregion
+    }
+}
